08f50fcaf6bf7954abb36104b3d3aefb8ba938df
iqbal <jukifze@gmail.com>
2026-03-13 15:17:12 +0700

    Menambahkan JS

--- assets/js/validasi.js
+++ assets/js/validasi.js
@@ -1,19 +1,15 @@
 document.getElementById("loginForm").addEventListener("submit", function(e){
 
-e.preventDefault();
+    e.preventDefault(); // supaya tidak reload
 
-let email = document.getElementById("email").value;
-let password = document.getElementById("password").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-if(email === "" || password === ""){
-    alert("Email dan Password harus diisi!");
-    return;
-}
-
-
-
-alert("Login berhasil!");
-
-window.location.href = "profile.html";
+    if(username === "w" && password === "w"){
+        alert("Login berhasil!");
+        window.location.href = "index.html"; // pindah ke halaman utama
+    } else {
+        alert("Username atau password salah");
+    }
 
 });
